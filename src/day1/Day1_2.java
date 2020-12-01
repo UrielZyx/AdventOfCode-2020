7e7ca21b8c295f3f54ef52834020b6c4083341cb
Uriel Zylbermann <Uriel.Zylbermann@exlibrisgroup.com>
2020-12-01 13:39:46 +0200

    day 1.2 Complete

---
+++ src/day1/Day1_2.java
@@ -0,0 +1,32 @@
+
+package day1;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import FileIO.InputReader;
+
+public class Day1_2 {
+
+	private static final String INPUT = "input.txt"; // "test input.txt";
+	private static final int TARGET = 2020;
+
+	public static void main(String[] args) {
+		Set<Integer> singles = new HashSet<>();
+		Map<Integer, Integer> doubles = new HashMap<>();
+
+		for (String v : InputReader.read(1, INPUT)) {
+			int n = Integer.parseInt(v);
+			int m = TARGET - n;
+			if (doubles.containsKey(m)) {
+				System.out.println(n * doubles.get(m) * (m - doubles.get(m)));
+			}
+			singles.add(n);
+			for (int o : singles) {
+				doubles.put(o + n, n);
+			}
+		}
+	}
+}
