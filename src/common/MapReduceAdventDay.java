c16aa6faffed1c472ffe77515050ef50296e9c6a
Uriel Zylbermann <Uriel.Zylbermann@exlibrisgroup.com>
2020-12-02 09:10:05 +0200

    Day 2

---
+++ src/common/MapReduceAdventDay.java
@@ -0,0 +1,19 @@
+package common;
+
+public abstract class MapReduceAdventDay extends AdventDay {
+
+	@Override
+	protected String runConcrete(String fileName) {
+		for (String l : InputReader.readLines(getDay(), fileName)) {
+			processLine(l);
+		}
+		return processResults();
+	}
+
+	protected abstract int getDay();
+
+	protected abstract void processLine(String line);
+
+	protected abstract String processResults();
+
+}
---
+++ src/day2/Day2_1.java
@@ -0,0 +1,39 @@
+package day2;
+
+import org.apache.commons.lang3.StringUtils;
+
+import common.MapReduceAdventDay;
+
+public class Day2_1 extends MapReduceAdventDay {
+
+	private int count = 0;
+
+	@Override
+	protected int getDay() {
+		return 2;
+	}
+
+	public static void main(String[] args) {
+		new Day2_1().testAndRun("2");
+	}
+
+	@Override
+	protected void processLine(String line) {
+		String[] parts = line.split("[- :]");
+		int min = Integer.parseInt(parts[0]);
+		int max = Integer.parseInt(parts[1]);
+		char letter = parts[2].charAt(0);
+		String password = parts[4];
+		int matches = StringUtils.countMatches(password, letter);
+
+		if (matches >= min && matches <= max) {
+			count++;
+		}
+	}
+
+	@Override
+	protected String processResults() {
+		return Integer.toString(count);
+	}
+
+}
