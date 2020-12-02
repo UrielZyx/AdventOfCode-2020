7848461ff9edcb1e45e19d50d870c9499ba58df1
Uriel Zylbermann <Uriel.Zylbermann@exlibrisgroup.com>
2020-12-02 09:30:35 +0200

    refactor day 1

--- src/day1/Day1_1.java
+++ src/day1/Day1_1.java
@@ -3,28 +3,40 @@ package day1;
 import java.util.HashSet;
 import java.util.Set;
 
-import common.AdventDay;
-import common.InputReader;
+import common.MapReduceAdventDay;
 
-public class Day1_1 extends AdventDay {
+public class Day1_1 extends MapReduceAdventDay {
 
 	private static final int TARGET = 2020;
+	Set<Integer> values = new HashSet<>();
+	String result = null;
 
 	public static void main(String[] args) {
 		new Day1_1().testAndRun("514579");
 	}
 
 	@Override
-	protected String runConcrete(String fileName) {
-		Set<Integer> values = new HashSet<>();
+	protected void clear() {
+		values = new HashSet<>();
+		result = null;
+	}
 
-		for (String v : InputReader.readLines(1, fileName)) {
-			int n = Integer.parseInt(v);
-			if (values.contains(TARGET - n)) {
-				return Integer.toString(n * (TARGET - n));
-			}
-			values.add(n);
+	@Override
+	protected int getDay() {
+		return 1;
+	}
+
+	@Override
+	protected void processLine(String line) {
+		int n = Integer.parseInt(line);
+		if (values.contains(TARGET - n)) {
+			result = Integer.toString(n * (TARGET - n));
 		}
-		return null;
+		values.add(n);
+	}
+
+	@Override
+	protected String processResults() {
+		return result;
 	}
 }
--- src/day1/Day1_2.java
+++ src/day1/Day1_2.java
@@ -6,12 +6,15 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import common.AdventDay;
 import common.InputReader;
+import common.MapReduceAdventDay;
 
-public class Day1_2 extends AdventDay {
+public class Day1_2 extends MapReduceAdventDay {
 
 	private static final int TARGET = 2020;
+	Set<Integer> singles = new HashSet<>();
+	Map<Integer, Integer> doubles = new HashMap<>();
+	String result = null;
 
 	public static void main(String[] args) {
 		new Day1_2().testAndRun("241861950");
@@ -19,9 +22,6 @@ public class Day1_2 extends AdventDay {
 
 	@Override
 	protected String runConcrete(String fileName) {
-		Set<Integer> singles = new HashSet<>();
-		Map<Integer, Integer> doubles = new HashMap<>();
-
 		for (String v : InputReader.readLines(1, fileName)) {
 			int n = Integer.parseInt(v);
 			int m = TARGET - n;
@@ -35,5 +35,35 @@ public class Day1_2 extends AdventDay {
 		}
 
 		return null;
+	}
+
+	@Override
+	protected void clear() {
+		singles = new HashSet<>();
+		doubles = new HashMap<>();
+		result = null;
+	}
+
+	@Override
+	protected int getDay() {
+		return 1;
+	}
+
+	@Override
+	protected void processLine(String line) {
+		int n = Integer.parseInt(line);
+		int m = TARGET - n;
+		if (doubles.containsKey(m)) {
+			result = Integer.toString(n * doubles.get(m) * (m - doubles.get(m)));
+		}
+		singles.add(n);
+		for (int o : singles) {
+			doubles.put(o + n, n);
+		}
+	}
+
+	@Override
+	protected String processResults() {
+		return result;
 	}
 }
