41d649b0b90aa4b42628f6b753f2adf64bac3f2d
Uriel Zylbermann <Uriel.Zylbermann@exlibrisgroup.com>
2020-12-02 09:19:03 +0200

    Day 2.2 complete

---
+++ src/day2/Day2_2.java
@@ -0,0 +1,40 @@
+package day2;
+
+import common.MapReduceAdventDay;
+
+public class Day2_2 extends MapReduceAdventDay {
+
+	private int count = 0;
+
+	@Override
+	protected void clear() {
+		count = 0;
+	}
+
+	@Override
+	protected int getDay() {
+		return 2;
+	}
+
+	public static void main(String[] args) {
+		new Day2_2().testAndRun("1");
+	}
+
+	@Override
+	protected void processLine(String line) {
+		String[] parts = line.split("[- :]");
+		int min = Integer.parseInt(parts[0]);
+		int max = Integer.parseInt(parts[1]);
+		char letter = parts[2].charAt(0);
+		String password = parts[4];
+
+		if (password.charAt(min - 1) == letter ^ password.charAt(max - 1) == letter) {
+			count++;
+		}
+	}
+
+	@Override
+	protected String processResults() {
+		return Integer.toString(count);
+	}
+}
